RickyGrassmuck/SporkBot.NET

Add consideration for easter eggs being enabled in settings, fix Suicune Change species rng for TradeCord, some bugfixes (I really need to rewrite this mess) Add check if we're using ListUtil for Giveaway instead of TradeCord. Amend commit since I'm squashing and force-pushing while bringing the fork in line with the main branch Add Giveaway module to Discord bot (#22)
Thanks, rigrassm.
Co-authored-by: Koi-3088 <61223145+Koi-3088@users.noreply.github.com>
Specify USB port instead of adding the first result (can be found via Device Manager).
Re-add boolean check because we don't want to fix everything
FixOT will attempt to regenerate illegal Pokémon.
Apply trash bytes for reasons.
Minor TradeCord fixes and adjustments.
Minor clean for C#9
Use "GetValidPreEvolutions()" instead of "GetPreEvolutions()".
Index forms correctly.
Fix the fixed and re-introduced empty daycare index error.
*an* Ultra Ball.
Add EvoTree breeding for TradeCord.
Remove unnecessary value declarations for pinging on encounter match.
Mildly beautify EncounterBot mark output.
Integrate Anubis' system update prevention into Soft Reset and Regigigas Encounter Modes.
Rename "Regi" Encounter Mode to "Soft Reset".
Speed up "A" clicks for Regigigas and Soft Reset modes.
Add Mark logging output for EncounterBot.
Fix oops (re-order logic, remove unnecessary lines).
Add optional species and form specification for $massrelease
Use an obscure string splitter because people like symbols in their names.
Fix things that broke after rebasing to the latest main repo commit.
Use a less unfortunate field name and value splitter...again.
Fix Marowak-Alola always generating as an NPC trade.
Add filters for "$list <species>" to narrow down results.
Fix Cherish Pichu and Octillery
Stop making dumb mistakes, me (implying the rest of it isn't a dumb mistake).
Can't breed antiques.
Use a less unfortunate embed name and value splitter
Add Melmetal-Gmax to TradeCord.
Add ability to search by caught ball.
Have MassRelease ignor

SysBot.Pokemon/BotEncounter/EncounterBot.cs
@@ -25,6 +25,8 @@ namespace SysBot.Pokemon
         }
 
         private int encounterCount;
+        private int catchCount;
+        private byte[] pouchData = { 0 };
 
         public override async Task MainLoop(CancellationToken token)
         {
@@ -37,12 +39,18 @@ namespace SysBot.Pokemon
             // Clear out any residual stick weirdness.
             await ResetStick(token).ConfigureAwait(false);
 
+            if (Hub.Config.StopConditions.CatchEncounter)
+                await CheckPokeBallCount(token).ConfigureAwait(false);
+
             var task = Hub.Config.Encounter.EncounteringType switch
             {
                 EncounterMode.VerticalLine => WalkInLine(token),
                 EncounterMode.HorizontalLine => WalkInLine(token),
                 EncounterMode.Eternatus => DoEternatusEncounter(token),
                 EncounterMode.LegendaryDogs => DoDogEncounter(token),
+                EncounterMode.SoftReset => DoSoftResetEncounter(token),
+                EncounterMode.Regigigas => DoSoftResetEncounter(token),
+                EncounterMode.SoJCamp => DoSoJCampEncounter(token),
                 _ => WalkInLine(token),
             };
             await task.ConfigureAwait(false);
@@ -201,7 +209,8 @@ namespace SysBot.Pokemon
         private async Task<bool> HandleEncounter(PK8 pk, bool legends, CancellationToken token)
         {
             encounterCount++;
-            Log($"Encounter: {encounterCount}{Environment.NewLine}{ShowdownSet.GetShowdownText(pk)}{Environment.NewLine}");
+            Log($"Encounter: {encounterCount}{Environment.NewLine}{ShowdownSet.GetShowdownText(pk)}{Environment.NewLine}{(StopConditionSettings.HasMark(pk, out RibbonIndex mark) ? $"Mark: {mark.ToString().Replace("Mark", "")}{Environment.NewLine}" : "")}");
+            TradeExtensions.EncounterLogs(pk);
             if (legends)
                 Counts.AddCompletedLegends();
             else
@@ -212,12 +221,17 @@ namespace SysBot.Pokemon
 
             if (StopConditionSettings.EncounterFound(pk, DesiredIVs, Hub.Config.StopConditions))
             {
-                Log("Result found! Stopping routine execution; restart the bot(s) to search again.");
+                Log(Hub.Config.StopConditions.CatchEncounter && (Hub.Config.Encounter.EncounteringType == EncounterMode.VerticalLine || Hub.Config.Encounter.EncounteringType == EncounterMode.HorizontalLine) ?
+                    "Result found! Attempting to catch..." : $"{(!Hub.Config.StopConditions.PingOnMatch.Equals(string.Empty) ? $"<@{Hub.Config.StopConditions.PingOnMatch}>\n" : "")}Result found! Stopping routine execution; restart the bot(s) to search again.");
                 if (Hub.Config.StopConditions.CaptureVideoClip)
                 {
                     await Task.Delay(Hub.Config.StopConditions.ExtraTimeWaitCaptureVideo, token).ConfigureAwait(false);
                     await PressAndHold(CAPTURE, 2_000, 1_000, token).ConfigureAwait(false);
                 }
+
+                if (Hub.Config.StopConditions.CatchEncounter && (Hub.Config.Encounter.EncounteringType == EncounterMode.VerticalLine || Hub.Config.Encounter.EncounteringType == EncounterMode.HorizontalLine))
+                    await CatchWildPokemon(pk, token).ConfigureAwait(false);
+
                 return true;
             }
 
@@ -237,6 +251,130 @@ namespace SysBot.Pokemon
             await Click(A, 0_400, token).ConfigureAwait(false);
             await Click(B, 0_400, token).ConfigureAwait(false);
             await Click(B, 0_400, token).ConfigureAwait(false);
+        }
+
+        private async Task CatchWildPokemon(PK8 pk, CancellationToken token)
+        {
+            var check = await ReadPokemon(WildPokemonOffset, token).ConfigureAwait(false);
+            if (encounterCount != 0 && encounterCount % catchCount == 0)
+            {
+                Log($"Ran out of Master Balls to catch {SpeciesName.GetSpeciesName(pk.Species, 2)}.");
+                if (Hub.Config.StopConditions.InjectPokeBalls)
+                {
+                    Log("Restoring original pouch data.");
+                    await Connection.WriteBytesAsync(pouchData, PokeBallOffset, token).ConfigureAwait(false);
+                    await Task.Delay(0_500, token).ConfigureAwait(false);
+                }
+                else
+                {
+                    Log($"{(!Hub.Config.StopConditions.PingOnMatch.Equals(string.Empty) ? $"<@{Hub.Config.StopConditions.PingOnMatch}>\n" : "")}Result found! Stopping routine execution; restart the bot(s) to search again.");
+                    return;
+                }
+            }
+
+            await SetLastUsedBall(Ball.Master, token).ConfigureAwait(false);
+            await Click(B, 1_000, token).ConfigureAwait(false);
+            await Click(X, 1_000, token).ConfigureAwait(false);
+            await Click(A, 3_000, token).ConfigureAwait(false); // Throw ball
+
+            await Click(B, 1_000, token).ConfigureAwait(false);
+            await Click(B, 1_000, token).ConfigureAwait(false); // Just in case we didn't
+
+            await Click(X, 1_000, token).ConfigureAwait(false);
+            await Click(A, 1_000, token).ConfigureAwait(false); // Attempt again to be sure
+            while (!await IsOnOverworld(Hub.Config, token).ConfigureAwait(false) && check.Species != 0)
+                await Click(B, 0_400, token).ConfigureAwait(false);
+
+            if (await IsOnOverworld(Hub.Config, token).ConfigureAwait(false) && !await IsInBattle(token).ConfigureAwait(false))
+            {
+                Log($"{(!Hub.Config.StopConditions.PingOnMatch.Equals(string.Empty) ? $"<@{Hub.Config.StopConditions.PingOnMatch}>\n" : "")}Caught {SpeciesName.GetSpeciesName(pk.Species, 2)}! Resuming routine...");
+                await WalkInLine(token).ConfigureAwait(false);
+            }
+        }
+
+        private async Task CheckPokeBallCount(CancellationToken token)
+        {
+            if (Hub.Config.StopConditions.CatchEncounter)
+            {
+                Log("Checking Poké Ball count...");
+                pouchData = await Connection.ReadBytesAsync(PokeBallOffset, 116, token).ConfigureAwait(false);
+                var counts = EncounterCount.GetBallCounts(pouchData);
+                catchCount = counts.PossibleCatches(Ball.Master);
+
+                if (catchCount == 0)
+                {
+                    Log("Insufficient Master Ball count. Please obtain at least one before starting.");
+                    return;
+                }
+            }
+        }
+
+        private async Task DoSoftResetEncounter(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested && Config.NextRoutineType == PokeRoutineType.EncounterBot)
+            {
+                while (!await IsInBattle(token).ConfigureAwait(false))
+                    await Click(A, 0_500, token).ConfigureAwait(false);
+
+                var pk = await ReadUntilPresent(Hub.Config.Encounter.EncounteringType == EncounterMode.SoftReset ? WildPokemonOffset : RaidPokemonOffset, 2_000, 0_200, token).ConfigureAwait(false);
+                if (pk == null)
+                {
+                    Log("Invalid data detected. Restarting loop.");
+                    continue;
+                }
+                else
+                {
+                    if (await HandleEncounter(pk, true, token).ConfigureAwait(false))
+                        return;
+                }
+
+                Log($"Resetting {SpeciesName.GetSpeciesNameGeneration(pk.Species, 2, 8)} by restarting the game");
+                await CloseGame(Hub.Config, token).ConfigureAwait(false);
+                Log("Restarting the game and mashing A until in battle!");
+                await StartGame(Hub.Config, token, true).ConfigureAwait(false);
+            }
+        }
+
+        private async Task DoSoJCampEncounter(CancellationToken token)
+        {
+            bool campEntered = false;
+            while (!token.IsCancellationRequested && Config.NextRoutineType == PokeRoutineType.EncounterBot)
+            {
+                await Click(X, 2_000, token).ConfigureAwait(false);
+                if (!campEntered)
+                {
+                    await Click(DDOWN, 0_600, token).ConfigureAwait(false);
+                    await Click(DRIGHT, 0_600, token).ConfigureAwait(false);
+                    campEntered = true;
+                }
+
+                Log("Entering camp...");
+                await Click(A, 12_000, token).ConfigureAwait(false);
+                await Click(B, 2_000, token).ConfigureAwait(false);
+                await Click(A, 0_500, token).ConfigureAwait(false);
+
+                while (!await IsInBattle(token).ConfigureAwait(false))
+                    await Task.Delay(2_000).ConfigureAwait(false);
+
+                var pk = await ReadUntilPresent(WildPokemonOffset, 2_000, 0_200, token).ConfigureAwait(false);
+                if (pk == null)
+                {
+                    Log("Invalid data detected. Restarting loop.");
+                    continue;
+                }
+                else
+                {
+                    if (await HandleEncounter(pk, true, token).ConfigureAwait(false))
+                        return;
+                }
+
+                Log("Fleeing from battle...");
+                while (await IsInBattle(token).ConfigureAwait(false))
+                    await FleeToOverworld(token).ConfigureAwait(false);
+
+                while (!await IsOnOverworld(Hub.Config, token).ConfigureAwait(false))
+                    await Task.Delay(2_000).ConfigureAwait(false);
+            }
         }
     }
 }

Z1R343L/Zygarde.NET

SysBot.Pokemon/BotEncounter/EncounterBot.cs
@@ -25,6 +25,8 @@ namespace SysBot.Pokemon
         }
 
         private int encounterCount;
+        private int catchCount;
+        private byte[] pouchData = { 0 };
 
         public override async Task MainLoop(CancellationToken token)
         {
@@ -37,12 +39,18 @@ namespace SysBot.Pokemon
             // Clear out any residual stick weirdness.
             await ResetStick(token).ConfigureAwait(false);
 
+            if (Hub.Config.StopConditions.CatchEncounter)
+                await CheckPokeBallCount(token).ConfigureAwait(false);
+
             var task = Hub.Config.Encounter.EncounteringType switch
             {
                 EncounterMode.VerticalLine => WalkInLine(token),
                 EncounterMode.HorizontalLine => WalkInLine(token),
                 EncounterMode.Eternatus => DoEternatusEncounter(token),
                 EncounterMode.LegendaryDogs => DoDogEncounter(token),
+                EncounterMode.SoftReset => DoSoftResetEncounter(token),
+                EncounterMode.Regigigas => DoSoftResetEncounter(token),
+                EncounterMode.SoJCamp => DoSoJCampEncounter(token),
                 _ => WalkInLine(token),
             };
             await task.ConfigureAwait(false);
@@ -201,7 +209,8 @@ namespace SysBot.Pokemon
         private async Task<bool> HandleEncounter(PK8 pk, bool legends, CancellationToken token)
         {
             encounterCount++;
-            Log($"Encounter: {encounterCount}{Environment.NewLine}{ShowdownParsing.GetShowdownText(pk)}{Environment.NewLine}");
+            Log($"Encounter: {encounterCount}{Environment.NewLine}{ShowdownParsing.GetShowdownText(pk)}{Environment.NewLine}{(StopConditionSettings.HasMark(pk, out RibbonIndex mark) ? $"Mark: {mark.ToString().Replace("Mark", "")}{Environment.NewLine}" : "")}");
+            TradeExtensions.EncounterLogs(pk);
             if (legends)
                 Counts.AddCompletedLegends();
             else
@@ -212,12 +221,17 @@ namespace SysBot.Pokemon
 
             if (StopConditionSettings.EncounterFound(pk, DesiredIVs, Hub.Config.StopConditions))
             {
-                Log("Result found! Stopping routine execution; restart the bot(s) to search again.");
+                Log(Hub.Config.StopConditions.CatchEncounter && (Hub.Config.Encounter.EncounteringType == EncounterMode.VerticalLine || Hub.Config.Encounter.EncounteringType == EncounterMode.HorizontalLine) ?
+                    "Result found! Attempting to catch..." : $"{(!Hub.Config.StopConditions.PingOnMatch.Equals(string.Empty) ? $"<@{Hub.Config.StopConditions.PingOnMatch}>\n" : "")}Result found! Stopping routine execution; restart the bot(s) to search again.");
                 if (Hub.Config.StopConditions.CaptureVideoClip)
                 {
                     await Task.Delay(Hub.Config.StopConditions.ExtraTimeWaitCaptureVideo, token).ConfigureAwait(false);
                     await PressAndHold(CAPTURE, 2_000, 1_000, token).ConfigureAwait(false);
                 }
+
+                if (Hub.Config.StopConditions.CatchEncounter && (Hub.Config.Encounter.EncounteringType == EncounterMode.VerticalLine || Hub.Config.Encounter.EncounteringType == EncounterMode.HorizontalLine))
+                    await CatchWildPokemon(pk, token).ConfigureAwait(false);
+
                 return true;
             }
 
@@ -237,6 +251,130 @@ namespace SysBot.Pokemon
             await Click(A, 0_400, token).ConfigureAwait(false);
             await Click(B, 0_400, token).ConfigureAwait(false);
             await Click(B, 0_400, token).ConfigureAwait(false);
+        }
+
+        private async Task CatchWildPokemon(PK8 pk, CancellationToken token)
+        {
+            var check = await ReadPokemon(WildPokemonOffset, token).ConfigureAwait(false);
+            if (encounterCount != 0 && encounterCount % catchCount == 0)
+            {
+                Log($"Ran out of Master Balls to catch {SpeciesName.GetSpeciesName(pk.Species, 2)}.");
+                if (Hub.Config.StopConditions.InjectPokeBalls)
+                {
+                    Log("Restoring original pouch data.");
+                    await Connection.WriteBytesAsync(pouchData, PokeBallOffset, token).ConfigureAwait(false);
+                    await Task.Delay(0_500, token).ConfigureAwait(false);
+                }
+                else
+                {
+                    Log($"{(!Hub.Config.StopConditions.PingOnMatch.Equals(string.Empty) ? $"<@{Hub.Config.StopConditions.PingOnMatch}>\n" : "")}Result found! Stopping routine execution; restart the bot(s) to search again.");
+                    return;
+                }
+            }
+
+            await SetLastUsedBall(Ball.Master, token).ConfigureAwait(false);
+            await Click(B, 1_000, token).ConfigureAwait(false);
+            await Click(X, 1_000, token).ConfigureAwait(false);
+            await Click(A, 3_000, token).ConfigureAwait(false); // Throw ball
+
+            await Click(B, 1_000, token).ConfigureAwait(false);
+            await Click(B, 1_000, token).ConfigureAwait(false); // Just in case we didn't
+
+            await Click(X, 1_000, token).ConfigureAwait(false);
+            await Click(A, 1_000, token).ConfigureAwait(false); // Attempt again to be sure
+            while (!await IsOnOverworld(Hub.Config, token).ConfigureAwait(false) && check.Species != 0)
+                await Click(B, 0_400, token).ConfigureAwait(false);
+
+            if (await IsOnOverworld(Hub.Config, token).ConfigureAwait(false) && !await IsInBattle(token).ConfigureAwait(false))
+            {
+                Log($"{(!Hub.Config.StopConditions.PingOnMatch.Equals(string.Empty) ? $"<@{Hub.Config.StopConditions.PingOnMatch}>\n" : "")}Caught {SpeciesName.GetSpeciesName(pk.Species, 2)}! Resuming routine...");
+                await WalkInLine(token).ConfigureAwait(false);
+            }
+        }
+
+        private async Task CheckPokeBallCount(CancellationToken token)
+        {
+            if (Hub.Config.StopConditions.CatchEncounter)
+            {
+                Log("Checking Poké Ball count...");
+                pouchData = await Connection.ReadBytesAsync(PokeBallOffset, 116, token).ConfigureAwait(false);
+                var counts = EncounterCount.GetBallCounts(pouchData);
+                catchCount = counts.PossibleCatches(Ball.Master);
+
+                if (catchCount == 0)
+                {
+                    Log("Insufficient Master Ball count. Please obtain at least one before starting.");
+                    return;
+                }
+            }
+        }
+
+        private async Task DoSoftResetEncounter(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested && Config.NextRoutineType == PokeRoutineType.EncounterBot)
+            {
+                while (!await IsInBattle(token).ConfigureAwait(false))
+                    await Click(A, 0_500, token).ConfigureAwait(false);
+
+                var pk = await ReadUntilPresent(Hub.Config.Encounter.EncounteringType == EncounterMode.SoftReset ? WildPokemonOffset : RaidPokemonOffset, 2_000, 0_200, token).ConfigureAwait(false);
+                if (pk == null)
+                {
+                    Log("Invalid data detected. Restarting loop.");
+                    continue;
+                }
+                else
+                {
+                    if (await HandleEncounter(pk, true, token).ConfigureAwait(false))
+                        return;
+                }
+
+                Log($"Resetting {SpeciesName.GetSpeciesNameGeneration(pk.Species, 2, 8)} by restarting the game");
+                await CloseGame(Hub.Config, token).ConfigureAwait(false);
+                Log("Restarting the game and mashing A until in battle!");
+                await StartGame(Hub.Config, token, true).ConfigureAwait(false);
+            }
+        }
+
+        private async Task DoSoJCampEncounter(CancellationToken token)
+        {
+            bool campEntered = false;
+            while (!token.IsCancellationRequested && Config.NextRoutineType == PokeRoutineType.EncounterBot)
+            {
+                await Click(X, 2_000, token).ConfigureAwait(false);
+                if (!campEntered)
+                {
+                    await Click(DDOWN, 0_600, token).ConfigureAwait(false);
+                    await Click(DRIGHT, 0_600, token).ConfigureAwait(false);
+                    campEntered = true;
+                }
+
+                Log("Entering camp...");
+                await Click(A, 12_000, token).ConfigureAwait(false);
+                await Click(B, 2_000, token).ConfigureAwait(false);
+                await Click(A, 0_500, token).ConfigureAwait(false);
+
+                while (!await IsInBattle(token).ConfigureAwait(false))
+                    await Task.Delay(2_000).ConfigureAwait(false);
+
+                var pk = await ReadUntilPresent(WildPokemonOffset, 2_000, 0_200, token).ConfigureAwait(false);
+                if (pk == null)
+                {
+                    Log("Invalid data detected. Restarting loop.");
+                    continue;
+                }
+                else
+                {
+                    if (await HandleEncounter(pk, true, token).ConfigureAwait(false))
+                        return;
+                }
+
+                Log("Fleeing from battle...");
+                while (await IsInBattle(token).ConfigureAwait(false))
+                    await FleeToOverworld(token).ConfigureAwait(false);
+
+                while (!await IsOnOverworld(Hub.Config, token).ConfigureAwait(false))
+                    await Task.Delay(2_000).ConfigureAwait(false);
+            }
         }
     }
 }